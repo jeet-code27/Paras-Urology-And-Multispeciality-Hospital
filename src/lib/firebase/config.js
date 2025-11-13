8879fe8c65b90597f301b4daef80228dd2f1f8e5
Your Name <jetendrasinghratore1@gmail.com>
2025-11-13 18:18:10 +0530

    comit

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||  "cast-api-key" ,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||  "cast-auth-domain" ,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||  " cast-project-id" ,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||  "cast-storage-bucket" ,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||  "cast-messaging-sender-id" ,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||  "cast-app-id" ,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ,
 };
 
 // Initialize Firebase
